ef4efd560ae2321dcc8a9e2636b9be37566d2b9e
JasonWu00 <zwu00@stuy.edu>
2020-01-02 23:30:02 -0500

    work to be finished

--- control.c
+++ control.c
@@ -10,6 +10,14 @@
 #include <errno.h>
 
 #define KEY 40000
+#define SEGSIZE 100
+
+union semun {
+  int val;
+  struct semid_ds *buf;
+  unsigned short *array;
+  struct seminfo *__buf;
+};
 
 int main(int element, char** args_array) {
   //printf("test\n");
@@ -21,40 +29,49 @@ int main(int element, char** args_array) {
   }
 
   else {//argument included
+    int sem_identifier = -1;
+    int shared_mem_identifier = -1;
 
-    if (strcmp(args_array[1], "-c") == 0) {
+    if (strcmp(args_array[1], "-c") == 0) {//create file
       //do -c things here
-      printf("TEST: you typed in -c.\n");
+      //printf("TEST: you typed in -c.\n");
+      int fd_story = -1;
+      fd_story = open("story.txt", O_RDONLY | O_TRUNC | O_CREAT, 0644);//makes the file
+      printf("File made\n");
+
+      sem_identifier = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644);//makes the semaphore
+      if (sem_identifier = -1) {//semget failed
+        printf("Error encountered: %i, %s\n", errno, strerror(errno));
+        printf("A debug is probably needed.\n");
+      }
+      else {
+        printf("Semaphore made\n");
+      }
+
+      shared_mem_identifier = shmget(KEY, SEGSIZE, IPC_CREAT | 0644);//makes the shared mem
+      printf("Shared memory made\n");
+      char* data = shmat(shared_mem_identifier, 0, 0);
+
       return 0;
     }
 
-    if (strcmp(args_array[1], "-v") == 0) {
+    if (strcmp(args_array[1], "-r") == 0) {//remove file
       //do -r things here
-      printf("TEST: you typed in -v.\n");
+      //printf("TEST: you typed in -r.\n");
+      printf("The story, before it is deleted:\n\n");
+      print_file_contents();
+      printf("Removing everything.\n");
+      semctl(sem_identifier, IPC_RMID, 0);
+      shmctl(shared_mem_identifier, IPC_RMID, 0);
+      printf("Semaphore, shared segment, and file removed.");
+      execl("/bin/rm", "rm", "-rf", "story.txt", NULL);
       return 0;
     }
 
-    if (strcmp(args_array[1], "-r") == 0) {
+    if (strcmp(args_array[1], "-v") == 0) {//view the file
       //do -v things here
       //printf("TEST: you typed in -v.\n");
-      int fd_story = -1;
-      fd_story = open("story.txt", O_RDONLY | O_CREAT, 0666);
-
-      if (fd_story == -1) {
-        printf("Error encountered: %i, %s\n", errno, strerror(errno));
-      }
-
-      printf("Printing story contents.\n\n");
-
-      int fd_stdin_backup = dup(stdin);
-      dup2(fd_story, 0);
-      char file[500];
-
-      FILE *fp = fgets(file, 500, stdin);
-      while (fp != NULL) { 
-        printf("%s", file);
-        fp = fgets(file, 500, stdin);
-      }
+      print_file_contents();
       return 0;
     }
 
@@ -67,4 +84,25 @@ int main(int element, char** args_array) {
   return 0;
 }
 
+void print_file_contents() {
+  int fd_story = -1;
+  fd_story = open("story.txt", O_RDONLY, 0644);
 
+  if (fd_story == -1) {
+    printf("Error encountered: %i, %s\n", errno, strerror(errno));
+  }
+
+  else {
+    printf("Printing story contents.\n\n");
+
+    int fd_stdin_backup = dup(stdin);
+    dup2(fd_story, 0);
+    char file[500];
+
+    FILE *fp = fgets(file, 500, stdin);
+    while (fp != NULL) {
+      printf("%s", file);
+      fp = fgets(file, 500, stdin);
+    }
+  }
+}
--- write.c
+++ write.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <errno.h>
+#include "control.h"
 
 #define KEY 40000
+#define SEGSIZE 100
 
 int main() {
-  
+  int shared_mem_identifier = shmget(KEY, SEGSIZE, 0);
+  char* data = shmat(shared_mem_identifier, 0, 0);
+
+  if (*data == NULL) {
+    printf("Error encountered: %i, %s\n", errno, strerror(errno));
+    return 0;
+  }
+
+  else {
+    printf("Current story:\n\n");
+    //print_file_contents();
+    printf("Your addition to the story: ");
+  }
   return 0;
 }
